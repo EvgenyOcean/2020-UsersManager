58854f75317cc2bec485871df9b17fc41e13d4d2
Evgeny Ocean <evgeny.ocean@gmail.com>
2020-10-05 20:11:32 +0300

    basic structure

--- main.py
+++ main.py
@@ -2,6 +2,51 @@ from flask import Flask, render_template, request, jsonify, request, redirect, u
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST', 'GET'])
+####################
+# Main Pages #
+####################
+
+# Home page 
+@app.route('/')
 def home():
-    return jsonify({'message': 'Hello World!'})
+    return render_template('home.html')
+
+# User listing page
+@app.route('/users')
+def users():
+    return render_template('list.html')
+
+# User profile page
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+#####################
+# REST APIs #
+#####################
+
+# Adding user
+@app.route('/api/create', methods=['POST'])
+def create_user():
+    pass
+
+# Deleting user by an id
+@app.route('/api/delete', methods=['DELETE'])
+def delete_user():
+    pass
+
+# Getting all the users
+@app.route('/api/users', methods=['GET'])
+def get_users_list():
+    pass
+
+# Getting user by an id
+@app.route('/api/user', methods=['GET'])
+def get_user():
+    pass
+
+# Editing user by an id
+@app.route('/api/profile', methods=['UPDATE'])
+def edit_user():
+    pass
+
